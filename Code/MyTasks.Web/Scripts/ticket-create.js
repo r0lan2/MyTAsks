bbe4cbd72467bf230f9b775ee96968d541056fb9
Rolando <rolandomartinezg@gmail.com>
2017-02-19 19:37:40 -0300

    remove section related with upload images or video.

--- Code/MyTasks.Web/Scripts/ticket-create.js
+++ Code/MyTasks.Web/Scripts/ticket-create.js
@@ -4,11 +4,23 @@
 
         function initialize() {
             $('#Content').summernote({
+                disableDragAndDrop: true,
+
                 focus: true,
                 height: 150,
                 codemirror: {
                     theme: 'united'
-                }
+                },
+                toolbar: [
+              ["style", ["style"]],
+              ["font", ["bold", "underline", "clear"]],
+              ["fontname", ["fontname"]],
+              ["color", ["color"]],
+              ["para", ["ul", "ol", "paragraph"]],
+              //["table", ["table"]],
+              //["insert", ["link", "picture", "video"]],
+              ["view", ["fullscreen", "codeview", "help"]]
+                ]
             });
         }
 
